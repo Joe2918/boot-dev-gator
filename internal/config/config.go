8d9b291bbc3e6437fbcc45e80f375820737c1174
Joenatan <Joenatan506@gmail.com>
2026-02-04 00:29:19 +0700

    Added ways to read, write username to gatorconfig.json

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,58 @@
 package config
 
-type config struct {
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
 	DbURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	jsonFile, err := os.ReadFile(fullPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config := Config{}
+	err = json.Unmarshal(jsonFile, &config)
+	return config, err
+}
+
+func (cfg Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(cfg)
+}
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	fullPath := filepath.Join(home, configFileName)
+	return fullPath, nil
+}
+
+func write(cfg Config) error {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	jsonConfig, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	os.WriteFile(fullPath, jsonConfig, 0644)
+	return nil
 }
