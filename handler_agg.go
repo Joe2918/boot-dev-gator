d8c00e54539dcb6391c7280456ec08f6da0ecec8
Joenatan <Joenatan506@gmail.com>
2026-02-24 11:36:32 +0700

    Added scrapeFeeds and updated agg command

--- handler_agg.go
+++ handler_agg.go
@@ -2,15 +2,61 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/bootdotdev/boot-dev-gator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <time between request>", cmd.Name)
+	}
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return err
+	}
+	ticker := time.NewTicker(timeBetweenRequests)
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+	feedToFetch, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Feed: %+v\n", feed)
+	fetchedTime := sql.NullTime{
+		Time:  time.Now().UTC(),
+		Valid: true,
+	}
+	args := database.MarkFeedFetchedParams{
+		ID:            feedToFetch.ID,
+		LastFetchedAt: fetchedTime,
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), args)
+	if err != nil {
+		return err
+	}
+
+	feed, err := fetchFeed(context.Background(), feedToFetch.Url)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(feedToFetch.Name)
+	fmt.Println("=====================================")
+	for _, item := range feed.Channel.Item {
+		fmt.Println(item.Title)
+	}
+
 	return nil
 }
--- handler_feed.go
+++ handler_feed.go
@@ -59,11 +59,16 @@ func handlerListFeeds(s *state, cmd command) error {
 		return fmt.Errorf("couldn't list feeds: %w", err)
 	}
 
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+
+	fmt.Printf("Found %d feeds:\n", len(feeds))
 	for _, feed := range feeds {
-		fmt.Println(feed.Name)
-		fmt.Println(feed.Url)
-		fmt.Println(feed.UserName)
-		fmt.Println()
+		fmt.Printf("* Feed:    %s\n", feed.Name)
+		fmt.Printf("* User:    %s\n", feed.UserName)
+		fmt.Println("=====================================")
 	}
 
 	return nil
@@ -75,5 +80,4 @@ func printFeed(feed database.Feed) {
 	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
 	fmt.Printf("* Name:          %s\n", feed.Name)
 	fmt.Printf("* URL:           %s\n", feed.Url)
-	fmt.Printf("* UserID:        %s\n", feed.UserID)
 }
