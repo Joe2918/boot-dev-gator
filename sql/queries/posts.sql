9dd3b94c4de612ea8bf37025a18d456200ec32ec
Joenatan <Joenatan506@gmail.com>
2026-02-25 16:22:47 +0700

    Added GetPosts query

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -11,3 +11,11 @@ VALUES (
     $6
 );
 
+-- name: GetPosts :many
+SELECT posts.*
+FROM posts
+INNER JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC, posts.created_at DESC
+limit $2;
+
